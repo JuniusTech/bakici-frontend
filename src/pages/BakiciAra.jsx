2ace6981143d0c559a4760bd44e1b4f1d8160c94
MEHMETTOKGOZ <42mt06@gmail.com>
2023-07-29 01:22:58 +0300

    Bakicikart ve pagination tamamlandı

--- src/pages/BakiciAra.jsx
+++ src/pages/BakiciAra.jsx
@@ -1,61 +1,65 @@
-import React from "react";
-import Bakici from "../images/Bakıcı1.png"
-import Yıldız from "../images/yıldız.png"
-import Location from "../images/location.png"
-import Clock from "../images/clock.png"
-import Money from "../images/money.png";
-import Kalp from "../images/kalp.png";
+import React, { useEffect, useState } from "react";
 import "../styles/bakiciara.css";
+import Bakicikart from "../components/Bakicikart";
+import ReactPaginate from "react-paginate";
 
 const BakiciAra = () => {
+  const items = [...Array(17).keys()];
+  const itemsPerPage = 8;
+
+  ///// Pagination Start//////
+  const [currentItems, setCurrentItems] = useState([]);
+  const [pageCount, setPageCount] = useState(0);
+  const [itemOffset, setItemOffset] = useState(0);
+
+  useEffect(() => {
+    // Fetch items from another resources.
+    const endOffset = itemOffset + itemsPerPage;
+    console.log(`Loading items from ${itemOffset} to ${endOffset}`);
+    setCurrentItems(items.slice(itemOffset, endOffset));
+    setPageCount(Math.ceil(items.length / itemsPerPage));
+  }, [itemOffset, items]);
+
+  // Invoke when user click to request another page.
+  const handlePageClick = (event) => {
+    const newOffset = event.selected * itemsPerPage;
+    console.log(`User requested page number ${event.selected}, which is offset ${newOffset}`);
+    setItemOffset(newOffset);
+  };
+
   return (
-    <div className="row w-75 ms-auto py-3">
-      <div className="d-grid col-xl-3 col-lg-4 col-md-6 my-xl-4 my-lg-3 my-md-2 my-sm-1 ">
-        <div className="card-body">
-          <div className="position-relative">
-            <img
-              src={Bakici}
-              className="card-img-top"
-              alt="..."
-            />
-            <div className="heart_container position-absolute">
-            <img src={Kalp} alt="" className="kalp_image position-absolute" />
-            </div>
+    <>
+      <div className="row">
+        {currentItems.map((item) => (
+          <div key={item} className="d-grid col-xl-3 col-lg-4 col-md-6 my-xl-4 my-lg-3 my-md-2 my-sm-1">
+            <Bakicikart />
           </div>
-          <div className="px-3">
-            <div className="bakici_name_container d-flex justify-content-between align-items-center m-1">
-              <div className="d-flex justify-content-center align-items-center">
-                <h5 className="card-title text-start ">Tuğba Aksoy</h5>
-              </div>
-              <div className="d-flex justify-content-center align-items-center m-1 px-1">
-                4.7 <img src={Yıldız} alt="" className="mx-1" /> (4)
-              </div>
-            </div>
-            <hr className="hr" />
-            <div className="bakici_info_container">
-              <div className="d-flex">
-                <img className="bakici_location_icon" src={Location} alt="" />
-                <p>Yenimahalle, Ankara</p>
-              </div>
-              <div className="d-flex">
-                <img className="bakici_clock_icon" src={Clock} alt="" />
-                <p>3 Yıl Deneyimli</p>
-              </div>
-              <div className="d-flex">
-                <img className="bakici_money_icon" src={Money} alt="" />
-                <p>Ücret 1000TL/Hafta</p>
-              </div>
-            </div>
-          </div>
-          <div className="card-body d-flex justify-content-end">
-            <a href="#" className="card-link">
-              İncele
-            </a>
-          </div>
-        </div>
+        ))}
       </div>
-    </div>
-  );
-};
+      <ReactPaginate
+      className="pagination"
+        forcePage={itemOffset / itemsPerPage}
+        nextLabel="next >"
+        onPageChange={handlePageClick}
+        pageRangeDisplayed={3}
+        marginPagesDisplayed={2}
+        pageCount={pageCount}
+        previousLabel="< previous"
+        pageClassName="page-item"
+        pageLinkClassName="page-link"
+        previousClassName="page-item"
+        previousLinkClassName="page-link"
+        nextClassName="page-item"
+        nextLinkClassName="page-link"
+        breakLabel="..."
+        breakClassName="page-item"
+        breakLinkClassName="page-link"
+        containerClassName="pagination"
+        activeClassName="active"
+        renderOnZeroPageCount={null}
+      />
+    </>
+  )
+}
 
 export default BakiciAra;
