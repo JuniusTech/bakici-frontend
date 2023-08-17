8d730345b9ec7a956176104d49bc4385b5cfb18d
MEHMETTOKGOZ <42mt06@gmail.com>
2023-08-18 01:26:06 +0300

    singleBakici page completed (not responsive)

--- src/components/BakiciDetail.jsx
+++ src/components/BakiciDetail.jsx
@@ -17,7 +17,10 @@ import EkServisIcon2 from "../images/EkServisIcon2.png";
 import TarihSecimi from "./TarihSecimi";
 import Select from "react-select";
 import useSelectOptions from "./select/useSelectOptions";
-import Kalp from "../images/RedHeart.png"
+import Yorumcu1 from "../images/yorumcu1.png";
+import Yorumcu2 from "../images/Yorumcu2.png";
+import ReactPaginate from "react-paginate";
+import { useEffect } from "react";
 // import { baseUrl } from "./config";
 
 function BakiciDetail() {
@@ -75,6 +78,31 @@ function BakiciDetail() {
     { value: "tum", label: "Pazar dahil tüm günler" },
   ];
 
+  const items = [...Array(17).keys()];
+  const itemsPerPage = 8;
+
+  ///// Pagination Start//////
+  const [currentItems, setCurrentItems] = useState([]);
+  const [pageCount, setPageCount] = useState(0);
+  const [itemOffset, setItemOffset] = useState(0);
+
+  useEffect(() => {
+    // Fetch items from another resources.
+    const endOffset = itemOffset + itemsPerPage;
+    console.log(`Loading items from ${itemOffset} to ${endOffset}`);
+    setCurrentItems(items.slice(itemOffset, endOffset));
+    setPageCount(Math.ceil(items.length / itemsPerPage));
+  }, [itemOffset]);
+
+  // Invoke when user click to request another page.
+  const handlePageClick = (event) => {
+    const newOffset = event.selected * itemsPerPage;
+    console.log(
+      `User requested page number ${event.selected}, which is offset ${newOffset}`
+    );
+    setItemOffset(newOffset);
+  };
+
   return (
     <div className="bakici-detail-container">
       <div className="bakici-detail-name-container">
@@ -92,13 +120,13 @@ function BakiciDetail() {
             <img src={Bakici1} alt="" className="d-block w-100" />
           </div>
         </Slider>
-        <div className="d-flex justify-content-around w-100 mt-3">
+        <div className="d-flex justify-content-between w-100 mt-3">
           <div className="d-flex justify-content-between align-items-center">
-            <img src={Star} alt="" />
-            <img src={Star} alt="" />
-            <img src={Star} alt="" />
-            <img src={Star} alt="" />
-            <img src={Star} alt="" />
+            <div class="rating_wrap justify-content-between">
+              <div class="rating">
+                <div class="product_rate"></div>
+              </div>
+            </div>
             <p className="mb-0 mx-4 bakici-detail-star-rate">4.7</p>
           </div>
           <div className="d-flex justify-content-between align-items-center">
@@ -124,7 +152,7 @@ function BakiciDetail() {
         </div>
         <div className="deneyim-container">
           <h5>DENEYİMLERİ</h5>
-          <div className="d-flex justify-content-between mb-4">
+          <div className="d-flex justify-content-between my-4">
             <div className="d-flex justify-content-center align-items-center">
               <img src={LanguageIcon} alt="" className="px-2" />
               <p className="">İyi seviye Ingilizce konusabiliyorum</p>
@@ -142,8 +170,8 @@ function BakiciDetail() {
         <div className="servisler-container">
           <h5>TUĞBANIN SERVİSLERİ</h5>
           <div>
-            <div className="d-flex justify-content-between">
-              <div className="d-block">
+            <div className="d-flex justify-content-between my-4">
+              <div className="d-block my-2">
                 <img src={SaatIcon} alt="" className="float-start px-2" />
                 <div className="tarife-container">
                   <h6>Gündüz Tarife</h6>
@@ -153,19 +181,19 @@ function BakiciDetail() {
                   </p>
                 </div>
               </div>
-              <div className="tarife-ücret">
+              <div className="tarife-ücret my-2">
                 <h4>30TL/Saati</h4>
               </div>
             </div>
-            <div className="d-flex justify-content-between">
-              <div className="d-block">
+            <div className="d-flex justify-content-between my-4">
+              <div className="d-block my-2">
                 <img src={GeceIcon} alt="" className="float-start px-2" />
                 <div className="tarife-container">
                   <h6>Gece Tarife</h6>
                   <p>Gece yatili cocuk bakimi tarifesi</p>
                 </div>
               </div>
-              <div className="tarife-ücret">
+              <div className="tarife-ücret my-2">
                 <h4>200TL/Gecesi</h4>
               </div>
             </div>
@@ -174,15 +202,15 @@ function BakiciDetail() {
 
         <div className="ek-servisler-container">
           <h5>TUĞBANIN EK SERVİSİ</h5>
-          <div>
-            <div className="d-flex justify-content-between">
-              <div className="d-block">
+          <div className="my-4">
+            <div className="d-flex justify-content-between my-2">
+              <div className="d-block my-2">
                 <img src={EkServisIcon1} alt="" className="float-start px-2" />
                 <p>Montesori egitimi / aktiviteleri</p>
               </div>
             </div>
-            <div className="d-flex justify-content-between">
-              <div className="d-block">
+            <div className="d-flex justify-content-between my-2">
+              <div className="d-block my-2">
                 <img src={EkServisIcon2} alt="" className="float-start px-3" />
                 <p className="ek-servis-content">
                   Ebeveyn isyerinden cocugu almak/ birakmak
@@ -191,36 +219,101 @@ function BakiciDetail() {
             </div>
           </div>
         </div>
-          <div className="gündüz-tarife-container">
-            <Select
-              className="tarife-select"
-              isMulti
-              closeMenuOnSelect={false}
-              hideSelectedOptions={false}
-              controlShouldRenderValue={false}
-              options={gündüzTarife}
-              isSearchable={false}
-              placeholder={
-                tarife.length
-                  ? `${tarife.length} Çalışma Şekli Seçildi`
-                  : "Gündüz Tarife"
-              }
-              components={{
-                Option: CheckboxOption,
-                ClearIndicator: null,
-              }}
-              styles={selectStyles}
-              onChange={handleChange}
-            />
+        <div className="gündüz-tarife-container">
+          <Select
+            className="tarife-select"
+            isMulti
+            closeMenuOnSelect={false}
+            hideSelectedOptions={false}
+            controlShouldRenderValue={false}
+            options={gündüzTarife}
+            isSearchable={false}
+            placeholder={
+              tarife.length
+                ? `${tarife.length} Çalışma Şekli Seçildi`
+                : "Gündüz Tarife"
+            }
+            components={{
+              Option: CheckboxOption,
+              ClearIndicator: null,
+            }}
+            styles={selectStyles}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="tarih-container">
+          <TarihSecimi onDatesSelected={handleDatesSelected} />
+        </div>
+        <div className="yorumlar-container">
+          <h5>YORUMLAR</h5>
+          <div className="yorumlar-box d-flex flex-column py-4">
+            <div className="name-star-info-container d-flex justify-content-between">
+              <div className="">
+                <p>Ahmet Firtina, 08-05-2023 </p>
+              </div>
+              <div className="d-flex flex-column">
+                <div class="rating_wrap align-self-end">
+                  <div class="rating">
+                    <div class="product_rate w-75"></div>
+                  </div>
+                </div>
+                <p>Gunduz mesai part-time- 1 ve 2 yas arasi</p>
+              </div>
+            </div>
+            <div>
+              <p>
+                Memnun kaldik, oglum ile kaliteli vakit gecirdiler. Montesori
+                aktiviteleri hosumuza gitti.
+              </p>
+            </div>
+            <img src={Yorumcu1} alt="" className="yorumcu-image" />
           </div>
-          <div className="tarih-container">
-          <TarihSecimi onDatesSelected={handleDatesSelected}  />
+          <div className="yorumlar-box d-flex flex-column py-4">
+            <div className="name-star-info-container d-flex justify-content-between">
+              <div className="">
+                <p>Ahmet Firtina, 08-05-2023 </p>
+              </div>
+              <div className="d-flex flex-column">
+                <div class="rating_wrap align-self-end">
+                  <div class="rating">
+                    <div class="product_rate w-75"></div>
+                  </div>
+                </div>
+                <p>Gunduz mesai part-time- 1 ve 2 yas arasi</p>
+              </div>
+            </div>
+            <div>
+              <p>
+                Memnun kaldik, oglum ile kaliteli vakit gecirdiler. Montesori
+                aktiviteleri hosumuza gitti.
+              </p>
+            </div>
+            <img src={Yorumcu1} alt="" className="yorumcu-image" />
           </div>
-          <div>
-            <h5>YORUMLAR</h5>
-          </div>
-          
+        </div>
       </div>
+      <ReactPaginate
+        className="pagination"
+        forcePage={itemOffset / itemsPerPage}
+        nextLabel=">"
+        onPageChange={handlePageClick}
+        pageRangeDisplayed={3}
+        marginPagesDisplayed={2}
+        pageCount={pageCount}
+        previousLabel="<"
+        pageClassName="page-item"
+        pageLinkClassName="page-link"
+        previousClassName="page-item"
+        previousLinkClassName="page-link"
+        nextClassName="page-item"
+        nextLinkClassName="page-link next-item"
+        breakLabel="..."
+        breakClassName="page-item"
+        breakLinkClassName="page-link"
+        containerClassName="pagination"
+        activeClassName="active"
+        renderOnZeroPageCount={null}
+      />
     </div>
   );
 }
