29ff5e301581595f8fb17328996963f04d4e2cbd
erenelmaci <222009541@ogr.uludag.edu.tr>
2023-10-04 23:17:15 +0300

    Yazim Hataları düzeltildi

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-import React from "react";
-import AppRouter from "./router/AppRouter";
+import React from "react"
+import AppRouter from "./router/AppRouter"
 
 function App() {
   return (
     <div className="App">
       <AppRouter />
-
-      
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
