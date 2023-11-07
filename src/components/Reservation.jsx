828998db27ac0c26f9c0b2a56bed5806f85975f9
Yasin Türk <yasinturk03@gmail.com>
2023-11-07 17:40:46 +0300

    feat:reservation-date-fixed

--- src/components/Reservation.jsx
+++ src/components/Reservation.jsx
@@ -5,25 +5,85 @@ import Select from "react-select";
 import useSelectOptions from "./select/useSelectOptions";
 import { servis, ages } from "../helper/options";
 import { useState } from "react";
+import { enUS, tr } from "date-fns/locale";
+import { format, isSameDay } from "date-fns";
+import { formatDate } from "../helper/utils";
+import { DateRangePickerCalendar, START_DATE } from "react-nice-dates";
+import { LuCalendarDays } from "react-icons/lu";
+import axios from "axios";
 
 const Reservation = () => {
   const { selectStyles, CheckboxOption, RadioOption } = useSelectOptions();
-  const [formData, setFormData] = useState({
-    selectedService: null,
-    selectedAgeGroup: null,
-    selectedDate: null,
-    message: "",
-  });
+  const [specialDays, setspecialDays] = useState([]);
+  const [specialDays2, setspecialDays2] = useState([]);
+  const [selectedDates, setSelectedDates] = useState([]);
+  const [available, setavailable] = useState(false);
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
+  const [serviceType, setServiceType] = useState(null);
+  const [ageGroup, setAgeGroup] = useState(null);
+  const [message, setMessage] = useState("");
+  const [focus, setFocus] = useState(START_DATE);
+  const [isCalendarOpen, setIsCalendarOpen] = useState(false);
 
-  const handleInputChange = (name, value) => {
-    setFormData({
-      ...formData,
-      [name]: value,
-    });
+  const baseURL = process.env.REACT_APP_BASE_URL;
+
+  const handleDateContainerClick = () => {
+    setIsCalendarOpen(!isCalendarOpen);
   };
 
-  const handleSubmit = () => {
-    console.log(formData);
+  const formData = {
+    serviceType: serviceType,
+    ageGroup: ageGroup,
+    startDate: formatDate(startDate),
+    endDate: formatDate(endDate),
+    message: message,
+  };
+
+  console.log("form--", formData);
+
+  const handleFocusChange = (newFocus) => {
+    setFocus(newFocus || START_DATE);
+  };
+
+  const modifiers = {
+    special: (date) =>
+      specialDays.some((day) => day.getTime() === date.getTime()),
+    customBackground: (date) => {
+      if (available) {
+      }
+      // Return true for the dates you want to change the background color of
+      // For example, let's change the background color of August 15th
+      return specialDays.some((day) => day.getTime() === date.getTime()); // (months are 0-based)
+    },
+    customBackground2: (date) => {
+      if (available) {
+      }
+      // Return true for the dates you want to change the background color of
+      // For example, let's change the background color of August 15th
+
+      return specialDays2.some((day) => day.getTime() === date.getTime()); // (months are 0-based)
+    },
+    selected: (date) =>
+      selectedDates.some((selectedDate) => isSameDay(selectedDate, date)),
+  };
+
+  const modifiersClassNames = {
+    highlight: "-highlight",
+    customBackground: "-customBackground",
+    customBackground2: "-customBackground2",
+  };
+
+  const handleSubmit = async () => {
+    try {
+      const res = await axios.post(
+        `https://carezone.onrender.com/meeting/create/653bb52fab18b6f3d99e8c57`,
+        formData
+      );
+      console.log("res--", res);
+    } catch (error) {
+      console.log(error.message);
+    }
   };
   return (
     <>
@@ -46,7 +106,7 @@ const Reservation = () => {
               styles={selectStyles}
               options={servis}
               onChange={(selectedOption) =>
-                handleInputChange("selectedService", selectedOption.value)
+                setServiceType(selectedOption.value)
               }
             />
           </div>
@@ -62,25 +122,45 @@ const Reservation = () => {
               isSearchable={false}
               styles={selectStyles}
               options={ages}
-              onChange={(selectedOption) =>
-                handleInputChange("selectedAgeGroup", selectedOption.value)
-              }
+              onChange={(selectedOption) => setAgeGroup(selectedOption.value)}
             />
           </div>
         </div>
 
         <div className="input_container">
           <p className="select_title">TARİH SEÇİNİZ</p>
-          <div className="bakici-kayit__datecontainer">
-            <input
-              className="bakici-kayit__date"
-              placeholder="Başlangıç Tarihi Seçiniz"
-              type="date"
-              onChange={(selectedOption) =>
-                handleInputChange("selectedDate", selectedOption.target.value)
-              }
-            />
+          <div
+            className="bakici-kayit__datecontainer"
+            onClick={handleDateContainerClick}
+          >
+            <span>
+              {format(startDate, "dd MMM yyyy", { locale: tr }) +
+                " - " +
+                format(startDate, "dd MMM yyyy", { locale: tr })}
+            </span>
+            <LuCalendarDays size={28} color="gray" />
           </div>
+          {isCalendarOpen && (
+            <div className="bakici-kayit__date">
+              <DateRangePickerCalendar
+                startDate={startDate}
+                endDate={endDate}
+                focus={focus}
+                onStartDateChange={setStartDate}
+                onEndDateChange={setEndDate}
+                onFocusChange={handleFocusChange}
+                locale={tr}
+                modifiers={modifiers}
+                modifiersClassNames={modifiersClassNames}
+              />
+              <button
+                className="reservation_button"
+                onClick={handleDateContainerClick}
+              >
+                EKLE
+              </button>
+            </div>
+          )}
         </div>
 
         <div className="textarea_container">
@@ -90,7 +170,7 @@ const Reservation = () => {
             className="reservation_textarea"
             rows={5}
             placeholder="Bakıcıya bildirmek istediğiniz bilgileri giriniz. Örneğin çocuğunuz ve ihtiyaçlarınız..."
-            onChange={(e) => handleInputChange("message", e.target.value)}
+            onChange={(e) => setMessage(e.target.value)}
           />
         </div>
 
